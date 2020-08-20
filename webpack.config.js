7608d653df02fccdcc594b3f6b2f57de9836c05d
Vladyslav <goodt1me@Vladyslavs-MacBook-Air.local>
2020-08-20 12:27:34 +0300

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const LiveReloadPlugin = require('webpack-livereload-plugin');
 const miniCss = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
 
 const options = {
 
@@ -12,19 +13,31 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    rules: [{
-       test:/\.(s*)css$/,
-       use: [
-          miniCss.loader,
-          'css-loader',
-          'sass-loader',
-       ]
-    }]
+    rules: [
+        {
+            test:/\.(s*)css$/,
+            use: [
+                miniCss.loader,
+                'css-loader',
+                'sass-loader',
+            ]
+        },
+        {
+            test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+            exclude: /node_modules/,
+            use: ['file-loader?name=[name].[ext]'], // ?name=[name].[ext] is only necessary to preserve the original file name
+            publicPath: 'assets/',
+            outputPath: 'assets/'
+        },
+    ]
  },
   plugins: [
     new miniCss({
         filename: 'style.css',
      }),
-     new LiveReloadPlugin(options)
+     new LiveReloadPlugin(options),
+     new HtmlWebpackPlugin({
+        template: './index.html'
+    })
   ]
 };
